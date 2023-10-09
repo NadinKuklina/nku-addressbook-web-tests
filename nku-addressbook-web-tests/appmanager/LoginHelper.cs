8a0b7aba2bc4f766aa31bbc3ca65b04703074153
Nadezhda Kuklina <nadezhda.kuklina@bimeister.com>
2023-10-10 00:40:05 +0300

    Лекция 5.2

--- nku-addressbook-web-tests/appmanager/LoginHelper.cs
+++ nku-addressbook-web-tests/appmanager/LoginHelper.cs
@@ -35,8 +35,8 @@ namespace WebAddressbookTests
         public bool IsLoggedIn(AccountData account)
         {
             return IsLoggedIn()
-                && driver.FindElement(By.Name("logout")).FindElement(By.TagName("b")).Text 
-                == System.String.Format("(${0})", account.Username);
+                && GetLoggedUserName() == account.Username;
+               
         }
 
         public bool IsLoggedIn()
@@ -50,6 +50,13 @@ namespace WebAddressbookTests
             {
                 driver.FindElement(By.LinkText("Logout")).Click();
             }            
-        }      
+        }    
+        
+        public string GetLoggedUserName()
+        {
+            string text = driver.FindElement(By.Name("logout")).FindElement(By.TagName("b")).Text;
+            return text.Substring(1, text.Length-2);
+              //  == System.String.Format("(${0})", account.Username);
+        }
     }
 }
